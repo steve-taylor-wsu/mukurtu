b8e84a8d4231bde6678a0f92cda8dc5819fb0552
Chacha Sikes <chachasikes@gmail.com>
2012-11-08 14:08:59 -0800

    Template for frontpage_app

---
+++ sites/all/themes/mukurtu_theme/templates/page--front.tpl.php
@@ -0,0 +1,27 @@
+<?php drupal_set_title($site_name);?>
+
+<div class="logo">
+  <?php echo '<img class="logo" src="' . $logo . '" />'; ?>
+</div>
+<div class="navigation">
+  <?php echo $frontpage['menu']; ?>
+</div>
+
+<?php
+
+
+
+foreach($frontpage['page_items'] as $item) {
+  $output = '';
+  $output .= t('<h3>' . $item['title'] . '</h3>' );
+  $output .= t('<div class="frontpage-content" id="' . $item['anchor'] . '">' . $item['content'] . '</div>' );
+  
+  if(!empty($item['data'])) {
+    $output .= $item['data'];
+  }
+   
+  echo $output;
+  
+}
+
+?>
